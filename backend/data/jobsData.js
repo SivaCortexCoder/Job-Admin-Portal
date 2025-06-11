abd4fb4fd88b0da907bfb458839e3748c7507298
SivaCortexCoder <sivaofficial76@gmail.com>
2025-06-11 20:48:35 +0530

    updated extra jobs

--- backend/data/jobsData.js
+++ backend/data/jobsData.js
@@ -80,6 +80,46 @@ const jobsData = [
      jobDescription: "A user-Friendly interface lets you browse stunning photos and videos",
     jobDescription1: "Filter destinations based on interests and travel style, and create personalized"
   },
+    {
+    id: 9,
+    jobLogo: 'https://res.cloudinary.com/dvgux7v3x/image/upload/v1749289338/Tesla_lsyyoc.png', 
+    jobTitle: 'React Developer (Contract)',
+    experience: 'Contract',
+    type: 'Hyderabad',
+    salary: '20k-30k',
+     jobDescription: "Responsible for building dynamic frontends using React and Redux. Contract role for a duration of 6 months.",
+    jobDescription1: ""
+  },
+    {
+    id: 10,
+    jobLogo: 'https://res.cloudinary.com/dvgux7v3x/image/upload/v1749289338/Tesla_lsyyoc.png', 
+    jobTitle: 'Data Analyst Intern',
+    experience: 'Internship',
+    type: 'Chennai',
+    salary: '30k-40k',
+     jobDescription: "Work with data science team to gather insights and generate reports using SQL, Python, and Tableau.",
+    jobDescription1: ""
+  },
+    {
+    id: 11,
+    jobLogo: 'https://res.cloudinary.com/dvgux7v3x/image/upload/v1749289338/Tesla_lsyyoc.png', 
+    jobTitle: 'Node Js Developer',
+    experience: 'Full-Time',
+    type: 'Onsite',
+    salary: '35k-40k',
+     jobDescription: "Develop scalable APIs and backend services. Flexible hours, ideal for developers with prior freelance experience.",
+    jobDescription1: ""
+  },
+    {
+    id: 12,
+    jobLogo: 'https://res.cloudinary.com/dvgux7v3x/image/upload/v1749289338/Tesla_lsyyoc.png', 
+    jobTitle: 'Cloud Engineer',
+    experience: 'Part-time',
+    type: 'Onsite',
+    salary: '12LPA',
+     jobDescription: "Manage Azure infrastructure and deployment pipelines. Help scale microservices and ensure cloud security best practices.",
+    jobDescription1: ""
+  },
 
 
 ];
